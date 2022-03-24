3ade133c5c528a642d30440d3b1fa578a7c2ef78
VitaliyKrivoshchapov <skrepach@gmail.com>
2022-03-24 17:26:15 +0500

    done object and correct string

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ class Movie(BaseModel):
     content_rating: str
     genre: str
     duration: int
-    actors_list: list[str]
-
-class Movie_List(BaseModel):
-    actors_list: str
+    #actors_list: str
 
 def convert_csv_json(file_name, out_path):
 
@@ -28,10 +25,10 @@ def convert_csv_json(file_name, out_path):
                     content_rating = row[2],
                     genre = row[3],
                     duration = row[4],
-                    #actors_list = list(row[5].replace(", u'", ' ').split(" "))
-                    actors_list = conver_string(row[5]),
-
+                    actors_list = list(row[5].replace(", u'", ' ').split(" "))
+                    #actors_list = conver_string(row[5])
                 )
+                print(movie)
                 js_txt_line = movie.json()
                 output_stream.write(js_txt_line)
                 output_stream.write("\n")
@@ -57,6 +54,20 @@ def convert_csv_json(file_name, out_path):
             print(a)
 
 
+def conver_string(str): # метод который на вход принимает строку и отдает правильный лист
+    list_str = list(str.split("u'"))
+    list_str.remove(list_str[0])
+    world = list(list_str.pop())  # last world и удалили из списка это слово
+    world.pop()
+    world.pop()
+    correct_world = "".join(world)
+
+    correct_list = get_correct_name(list_str)
+    correct_list.append(correct_world)
+    print(correct_list)#  это лист
+    return correct_list
+
+
 def get_correct_name(list_world):
     correct_list_world = []
     i = 0
@@ -70,24 +81,14 @@ def get_correct_name(list_world):
         i += 1
     return correct_list_world
 
-def conver_string(str): # метод который на вход принимает строку и отдает правильный лист
-    list_str = list(str.split("u'"))
-    list_str.remove(list_str[0])
-    world = list(list_str.pop())  # last world и удалили из списка это слово
-    world.pop()
-    world.pop()
-    correct_world = "".join(world)
 
-    correct_list = get_correct_name(list_str)
-    correct_list.append(correct_world)
-    print(correct_list)#  это лист
 
 def test_converter():
     input_file_name = "move.csv"
     output_file_name = "newMovieJson2.json"
     #test_list(input_file_name)
-    conver_string("[u'Tim Robbins', u'Morgan Freeman', u'Bob Gunton']")
-    #convert_csv_json(input_file_name, output_file_name)
+    #conver_string("[u'Tim Robbins', u'Morgan Freeman', u'Bob Gunton']")
+    convert_csv_json(input_file_name, output_file_name)
 
 
 if __name__ == "__main__":
